3718d9f7817c308858d961750d35db3059921a01
ilimeza <ilianameza530@gmail.com>
2019-10-08 14:25:01 +0100

    attempt to redirect nonuser to sign up page Co-authored-by: valeser <balerty84@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to user_session_path
+    end
+  end 
+
   def after_sign_in_path_for(user)
-      posts_path
-      end
+    posts_path
+  end
 
   def after_update_path_for(user)
-        posts_path
-          end
+    posts_path
+  end
+
+  
 
 #   def after_sign_in_path_for(resource)
 #   stored_location_for(resource) || welcome_path
 # end
+before_action :authenticate_user!
 
 end
