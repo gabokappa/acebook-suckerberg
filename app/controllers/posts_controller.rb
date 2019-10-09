13dbc2a5233e98610ed810cfd426aa1111b49494
Gabriel Gonzalez <gabokappa@gmail.com>
2019-10-09 16:55:14 +0100

    Spiked 'edit' only works with author Co-authored Iliana Meza <ilianameza530@gmail.com

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,18 +11,26 @@ class PostsController < ApplicationController
   end
 
   def index
-    p @user
     @posts = Post.all
   end
 
   def edit
     @post = Post.find(params[:id])
-    # redirect_to edit_post_url
+    if current_user.id != @post.user_id
+      flash[:notice] = "You can't edit this message"
+      redirect_to posts_url
+    end
   end
 
   def update
     a = params[:id]
     @post = Post.find_by(id: a)
+
+    if current_user.id != @post.user_id
+      flash[:notice] = "You can't edit this message"
+      redirect_to posts_url
+    end
+
     @post.update(post_params)
     redirect_to posts_url
   end
