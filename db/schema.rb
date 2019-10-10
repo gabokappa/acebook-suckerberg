a9cf97ecbabccb748176f4d58f16a9b35ac7f981
Gabriel Gonzalez <gabokappa@gmail.com>
2019-10-10 11:17:15 +0100

    Comments now have user_id and post_id reference. this had to be reflected in the comment model.s Co-authored-by: Eduard Kulcsickij-Gut <eduard.kulcsickij.gut@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191010095339) do
+ActiveRecord::Schema.define(version: 20191010100935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 20191010095339) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -44,5 +48,7 @@ ActiveRecord::Schema.define(version: 20191010095339) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
