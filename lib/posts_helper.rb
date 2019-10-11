738162319dc861ad614cecba8c577f979ec06679
Valeria Serci <balerty84@gmail.com>
2019-10-11 15:33:43 +0100

    Add likes for comments. Co-author V-C-B

--- lib/posts_helper.rb
+++ lib/posts_helper.rb
@@ -12,4 +12,18 @@ module PostsHelper
     end
     return @likes_posts
   end
+
+  def likes_all_comments
+    @comments = Comment.all
+    @likes = Like.all
+    @likes_comments = {}
+    @comments.each do |comment|
+      like_counter = 0
+      @likes.where(comment_id: comment.id).each do |like|
+        like_counter += 1
+      end
+      @likes_comments[comment.id] = like_counter
+    end
+    return @likes_comments
+  end
 end
--- spec/features/users_can_like_posts_spec.rb
+++ spec/features/users_can_like_posts_spec.rb
@@ -19,4 +19,16 @@ feature "Likes", type: :feature do
     click_link("#{ Emoji.find_by_alias("+1").raw }", :match => :first)
     expect(page).to have_content("#{ Emoji.find_by_alias("+1").raw } 2")
   end
+  scenario "user can like and dislike a post" do
+    user = FactoryBot.create(:user)
+    login_as(user, :scope => :user)
+    visit "/posts"
+    click_link 'New post'
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    click_link("#{ Emoji.find_by_alias("+1").raw }", :match => :first)
+    expect(page).to have_content("#{ Emoji.find_by_alias("+1").raw } 1")
+    click_link("#{ Emoji.find_by_alias("+1").raw }", :match => :first)
+    expect(page).to have_content("#{ Emoji.find_by_alias("+1").raw } 0")
+  end
 end
