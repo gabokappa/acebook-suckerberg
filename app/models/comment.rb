55a71397b7c2edd2899bc230a44563812f5cdf68
Gabriel Gonzalez <gabokappa@gmail.com>
2019-10-12 10:06:44 +0100

    Deletion of posts and comments are no longer raising errors due to foreign key constraints

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  has_many :likes
+  has_many :likes, dependent: :delete_all
 end
--- db/schema.rb
+++ db/schema.rb
@@ -60,10 +60,10 @@ ActiveRecord::Schema.define(version: 20191010110513) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
-  add_foreign_key "likes", "comments"
-  add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users"
-  add_foreign_key "posts", "users"
+  add_foreign_key "comments", "posts", on_delete: :cascade
+  add_foreign_key "comments", "users", on_delete: :cascade
+  add_foreign_key "likes", "comments", on_delete: :cascade
+  add_foreign_key "likes", "posts", on_delete: :cascade
+  add_foreign_key "likes", "users", on_delete: :cascade
+  add_foreign_key "posts", "users", on_delete: :cascade
 end
