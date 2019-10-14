80a51c7201af408889f44af758a0e0ee2ef1995c
Volker Bergner <volker@bergner.one>
2019-10-14 11:26:09 +0100

    active storage configuration - data model pening

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # set storage to test
+  config.active_storage.service = :test
 end
