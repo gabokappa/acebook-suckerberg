42e14ea3105a5db86f91133534f867b008208e8f
Gabriel Gonzalez <gabokappa@gmail.com>
2019-10-16 10:13:30 +0100

    Deleted references to un-used wall controller Co-authored by Iliana Meza <ilianameza530@gmail.com>

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 describe LikesController, type: :controller do
-  describe "GET /new " do
+  describe 'GET /new ' do
     login_user
-    it "responds with 200" do
+    it 'responds with 200' do
       get :new
       expect(response).to have_http_status(200)
     end
   end
 
-  describe "POST /" do
+  describe 'POST /' do
     login_user
-    it "creates a like for a post" do
+    it 'creates a like for a post' do
       new_post = FactoryBot.create(:post)
       # allow(:create).to receive(:find_post_wall) { 1 }
       post :create, params: { like: { liked: 1, post_id: 1}, wall_id: 1}
@@ -19,18 +19,18 @@ describe LikesController, type: :controller do
       DatabaseCleaner.clean
     end
   end
-  describe "POST /" do
+  describe 'POST /' do
     login_user
-    it "creates a like for a comment" do
+    it 'creates a like for a comment' do
       new_comment = FactoryBot.create(:comment)
       post :create, params: { like: { liked: 1 , comment_id: new_comment.id}}
       DatabaseCleaner.clean
     end
   end
 
-  describe "POST /" do
+  describe 'POST /' do
     login_user
-    it "creates a like for a post without form" do
+    it 'creates a like for a post without form' do
       new_post = FactoryBot.create(:post)
       get :create_no_form_posts, params: { post_id: new_post.id}
       expect(Like.all[0]).to be
@@ -38,9 +38,9 @@ describe LikesController, type: :controller do
     end
   end
 
-  describe "POST /" do
+  describe 'POST /' do
     login_user
-    it "creates a like for a comment without form" do
+    it 'creates a like for a comment without form' do
       new_comment = FactoryBot.create(:comment)
       get :create_no_form_comments, params: { comment_id: new_comment.id }
       expect(Like.all[0]).to be
--- spec/features/user_cannot_edit_comment_after_10mins_spec.rb
+++ spec/features/user_cannot_edit_comment_after_10mins_spec.rb
@@ -1,21 +1,23 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-feature "Not able to edit a comment after 600 seconds limit", type: :feature do
-  scenario "Can delete comment" do
+feature 'Not able to edit a comment after 600 seconds limit', type: :feature do
+  scenario 'Can delete comment' do
     user = FactoryBot.create(:user)
-    login_as(user, :scope => :user)
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, friends!"
-    click_button "Submit"
-    click_link "Comment"
-    fill_in "Message", with: "I am a comment"
-    click_button "Submit"
-    click_link "Logout"
+    login_as(user, scope: :user)
+    visit '/posts'
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello, friends!'
+    click_button 'Submit'
+    click_link 'Comment'
+    fill_in 'Message', with: 'I am a comment'
+    click_button 'Submit'
+    click_link 'Logout'
     Timecop.travel(Time.now + 601)
-    login_as(user, :scope => :user)
-    visit "/posts"
-    click_link "Edit comment"
+    login_as(user, scope: :user)
+    visit '/posts'
+    click_link 'Edit comment'
     expect(page).to have_content('ERROR')
     expect(page).to have_content('I am a comment')
   end
