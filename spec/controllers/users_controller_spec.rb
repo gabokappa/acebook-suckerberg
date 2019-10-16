e49393f69c6257d2c6cfed82a80834b7cb1d6546
ilimeza <ilianameza530@gmail.com>
2019-10-16 10:02:25 +0100

    add feature and controller test Co-authored-by: Gabriel Gonzalez <gabokappa@gmail.com>

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+    describe "GET /index " do
+        login_user
+        it "responds with 200" do
+          get :index
+          expect(response).to have_http_status(200)
+        end
+    end 
 
+    # describe "GET /show" do 
+    #     it "displays the users wall" do 
+    #         new_user = FactoryBot.create(:user)
+
+    #         login_as(new_user, scope: :new_user)
+    #          userswall_path
+    #         page.should have_content(user.email)
+    #     end 
+    # end
+    
 end
