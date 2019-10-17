2e521801d8cb15e72b2601cf85ea858b7f2055fc
Volker Bergner <volker@bergner.one>
2019-10-17 16:11:39 +0100

    all likes methods

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
 
+  describe 'likes_all_comments' do
+    login_user
+    it 'responds with correct hash' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      post1 = FactoryBot.create(:post)
+      post :create, params: { comment: {message: 'Hello, like!', wall_id: 1, user_id: post1.user.id, post_id: post1.id, post_test: post1.id }}
+      Like.create(comment_id: Comment.all[0].id, liked:1, user_id: user.id)
+      post :create, params: { comment: {message: 'Hello, no like!', wall_id: 1, user_id: post1.user.id, post_id: post1.id, post_test: post1.id }}
+      expect(likes_all_comments[Comment.all[0].id]).to eq(1)
+      expect(likes_all_comments[Comment.all[1].id]).to eq(0)
+    end
+  end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 require 'rails_helper'
+require './lib/posts_helper'
+include PostsHelper
+
 
 describe PostsController, type: :controller do
   describe 'GET /new ' do
@@ -28,6 +31,17 @@ describe PostsController, type: :controller do
     it 'responds with 200' do
       get :index
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'likes_all_posts' do
+    login_user
+    it 'responds with correct hash' do
+      user = FactoryBot.create(:user)
+      post :create, params: { post: { message: 'Hello, like!', wall_id: 1 } }
+      like = FactoryBot.create(:like)
+      expect(likes_all_posts[1]).to eq(1)
+      expect(likes_all_posts[Post.all[0].id]).to eq(0)
     end
   end
 end
