66c5537b53a062cea3bf46ce7596008ceb32884a
Gabriel Gonzalez <gabokappa@gmail.com>
2019-10-17 21:14:44 +0100

    fixed lack of profile pictures crashing the site and refactored some controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
-  def after_sign_in_path_for(user)
+  def after_sign_in_path_for(_user)
     userswall_path
   end
 
-  def after_update_path_for(user)
+  def after_update_path_for(_user)
     userswall_path
   end
 
@@ -15,8 +17,12 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:profile_pic, :email, :password, :password_confirmation)}
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:profile_pic, :email, :password, :password_confirmation)
+    end
 
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:profile_pic, :email, :password, :password_confirmation)}
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:profile_pic, :email, :password, :password_confirmation)
+    end
   end
 end
