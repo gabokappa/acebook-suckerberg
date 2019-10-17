871d3221f221bd86b0df92a0d07004f1d7d5b4c2
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-10-17 11:36:48 +0100

    user name added to the sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,10 +15,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:profile_pic, :email, :password, :password_confirmation)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:profile_pic, :email, :password, :password_confirmation, :username)}
 
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:profile_pic, :email, :password, :password_confirmation)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:profile_pic, :email, :password, :password_confirmation, :username)}
   end
-
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_14_151637) do
+ActiveRecord::Schema.define(version: 2019_10_17_094226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,8 +85,10 @@ ActiveRecord::Schema.define(version: 2019_10_14_151637) do
     t.datetime "confirmation_sent_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
