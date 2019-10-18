7ddc3dbe7257d0c2a9f61ba4cb9f8c8ee80ff7bb
Gabriel Gonzalez <gabokappa@gmail.com>
2019-10-18 10:54:12 +0100

    Rounded profile pics and fixed post feed order test

--- spec/features/posts/user_can_see_newest_post_first_spec.rb
+++ spec/features/posts/user_can_see_newest_post_first_spec.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-# feature 'Posts', type: :feature do
-#   scenario 'shows newest post first' do
-#     user = FactoryBot.create(:user)
-#     login_as(user, scope: :user)
-#     visit '/userswall'
-#     click_link 'New post'
-#     fill_in 'Message', with: 'Hello, world!'
-#     click_button 'Submit'
-#     visit '/posts'
-#     click_link 'New post'
-#     fill_in 'Message', with: 'Newest message'
-#     click_button 'Submit'
-#     expect(page.find('p:nth-child(30)')).to have_content "Newest message"
-#   end
-# end
+feature 'Posts', type: :feature do
+  scenario 'shows newest post first' do
+    user = FactoryBot.create(:user)
+    login_as(user, scope: :user)
+    visit '/userswall'
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello, world!'
+    click_button 'Submit'
+    visit '/posts'
+    click_link 'New post'
+    fill_in 'Message', with: 'Newest message'
+    click_button 'Submit'
+    expect(page.find('p:nth-child(9)')).to have_content "Newest message"
+  end
+end
